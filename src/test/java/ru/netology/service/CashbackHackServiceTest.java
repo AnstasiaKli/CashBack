6aef45e3a5f7016d479d8035ea53ee020cf608ef
Anastasia <asiaa2110@gmail.com>
2023-03-29 14:12:08 +0300

    tests added

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,14 +1,14 @@
 package ru.netology.service;
 
-import org.testng.annotations.Test;
+import org.junit.Test;
 
-import static org.testng.Assert.*;
+import static org.junit.Assert.*;
 
 public class CashbackHackServiceTest {
 
     CashbackHackService service =new CashbackHackService();
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void testRemain() {
         int actual = service.remain(700);
         int expected = 300;
